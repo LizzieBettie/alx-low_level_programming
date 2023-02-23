0628e7545cb17218d2ed325abd200bb61293f54b
Lizzie <myemail.com>
2023-02-22 18:50:54 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+/**
+ * print_alphabet - entry point
+ * Description: function that prints the alphabet, in lowercase,
+ * followed by a new line
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+
+}
