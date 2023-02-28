759cece124a4244ba309577c8de4f02836cb6178
Lizzie <myemail.com>
2023-02-27 17:33:51 -0800

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <unistd.h>
 int _putchar(char c);
 void reset_to_98(int *n);
-voi	 swap_int(int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
